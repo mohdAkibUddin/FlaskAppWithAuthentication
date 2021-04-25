753eab7c19bfb20315ac9f910ad921e9d1e42f98
mohd.akib.uddin <mohd.akib.uddin@gmail.com>
2021-04-25 18:51:40 -0400

    task: created route to homepage in app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from flask import render_template
+from forms import NewForm, EditForm
 
 app = Flask(__name__)
+SECRET_KEY = "notSoSecretKey"
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    return render_template(
+        "home.jinja2",
+        title="Actor's and their Oscars",
+        description="Details about actors and what age they were awarded an Oscar."
+    )
 
 
 if __name__ == '__main__':
