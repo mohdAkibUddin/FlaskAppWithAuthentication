117786d2998656efe26ae1eeb22cce45c88d8ccd
mohd.akib.uddin <mohd.akib.uddin@gmail.com>
2021-04-29 03:05:00 -0400

    task: database uri for login

--- Application/config.py
+++ Application/config.py
@@ -4,3 +4,11 @@ MYSQL_DATABASE_USER = "root"
 MYSQL_DATABASE_PASSWORD = "password"
 MYSQL_DATABASE_PORT = 3306
 MYSQL_DATABASE_DB = "oscarsData"
+
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
